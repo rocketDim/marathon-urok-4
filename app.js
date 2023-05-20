95a21b8a17f57e84e2d79eacafda596429e8789c
rocketDim <dmitriydem81@mail.ru>
2023-05-20 17:02:02 +0300

    немного допилил

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
-square.addEventListener('mousemove', () => setColor(square))
-square.addEventListener('mouseleave', () => removeColor(square))
+square.addEventListener('mousemove', setColor)
+square.addEventListener('mouseleave', removeColor)
 
     board.append(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+    const element = event.target
     const color = getRandomColor()
     element.style.backgroundColor = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
+function removeColor(event) {
+    const element = event.target
     element.style.backgroundColor = '#1d1d1d'
     element.style.boxShadow = `0 0 2px #000`
 }
 
 function getRandomColor() {
-    const index = Math.floor(Math.random() * colors.length)
-
-    return colors[index]
+    return colors[Math.floor(Math.random() * colors.length)]
 }
